1a3e50a75137746079fd9f0daf9ba970aacb9b50
ankitjain0748 <ankitjain0748@gmail.com>
2025-06-01 00:19:25 +0530

    first commit

--- src/Admin/common/ContactIcons.jsx
+++ src/Admin/common/ContactIcons.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BsWhatsapp } from "react-icons/bs";
-
 import { FaPhoneAlt } from "react-icons/fa";
 
 const ContactIcons = ({ phoneNumber }) => {
@@ -13,18 +12,22 @@ const ContactIcons = ({ phoneNumber }) => {
         href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`}
         target="_blank"
         rel="noopener noreferrer"
-        className=" d-flex align-items-center gap-2 px-1 py-1 rounded"
+        className="d-flex align-items-center justify-content-center  bg-success bg-gradient
+ text-white rounded-circle p-2 shadow-sm"
+        style={{ transition: 'all 0.3s ease' }}
       >
-        <BsWhatsapp size={24} />
+        <BsWhatsapp size={20} />
       </a>
 
       {/* Phone Icon */}
       <a
         href={`tel:${phoneNumber}`}
         target="_blank"
-        className="  d-flex align-items-center gap-2  px-1 py-1 rounded"
+        rel="noopener noreferrer"
+        className="d-flex align-items-center justify-content-center bg-primary bg-gradient   text-white rounded-circle p-2 shadow-sm"
+        style={{ transition: 'all 0.3s ease' }}
       >
-        <FaPhoneAlt size={24} />
+        <FaPhoneAlt size={18} />
       </a>
     </div>
   );
